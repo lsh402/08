a42c80910df3701a5b6211ecc018ae8eb57d53c9
h1071553866 <h1071553866@163.com>
2018-11-24 17:11:20 +0800

    源文件

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
 
     # 获取所有的要运行的方法（scripts文件夹下的所有继承TestCase的test开头的方法）
     suite = unittest.defaultTestLoader.discover("./scripts", "test_*.py")
-    # 运行！
+    # 运行！1111111
     BaseReport().run_suite_with_report(suite)
